3f1d0c236f78167f3b7a87df4dfacfd13aaf25fd
Shahzaib <zaib@info.com>
2025-06-17 16:23:08 +0500

    feat: implement append method for Circular Singly Linked List

--- csll.py
+++ csll.py
@@ -12,3 +12,17 @@ class CSLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+
+    def append(self, value):
+        new_node = Node(value)
+                                       
+        if not self.head:                       # If the CSLL is empty.
+            self.head = self.tail =new_node
+            new_node.next = new_node
+        else:                                   # If the CSLL has at least one node.
+            self.tail.next = new_node
+            new_node.next = self.head
+            self.tail = new_node
+
+        self.length += 1
+
