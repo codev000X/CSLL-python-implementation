73425c4599ed523995f5d40571e4fbf6827e4d05
Shahzaib <zaib@info.com>
2025-06-18 07:31:31 +0500

    feat: added CSLL delete_by_value with full edge case handling 🎯

--- csll.py
+++ csll.py
@@ -6,13 +6,13 @@ class Node:
     def __str__(self):
         return str(self.value)
 
-class CSLinkedList:
+class CSLinkedList: 
     
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
-
+  
     def __str__(self):
 
         if not self.head:
@@ -54,3 +54,41 @@ class CSLinkedList:
             self.head = new_node
             self.tail.next = self.head
         self.length += 1
+
+    def delete_by_value(self, value):
+        if not self.head:
+            return None
+
+        # making the dummy node and updating the loop accordingly.
+        dummy = Node(-99)
+        dummy.next = self.head
+        self.head = dummy
+        self.tail.next = self.head
+
+        # setting up the logic.
+        pre = dummy
+        curr = pre.next
+
+        while curr:
+            if curr.value == value:
+
+                if curr is  self.tail:
+                    self.tail = pre
+                    pre.next = self.head
+                else:
+                    next = curr.next
+                    pre.next = next
+
+                curr.next = None    
+                self.length -= 1
+                self.head = self.head.next
+                break
+
+            else:
+                pre = curr
+                curr = curr.next
+
+            if curr == self.head:
+                return "Node with mentioned value didn't found."
+
+        return self.head
